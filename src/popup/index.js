5f3ebc94758f59a4988555e6ac78a9f5bf711c91
C19 <classone2010@gmail.com>
2018-09-20 23:14:18 +0800

    add save data to server using graphql

--- src/popup/index.js
+++ src/popup/index.js
@@ -1,17 +1,26 @@
 import Vue from 'vue'
 import VueHighlightJS from 'vue-highlightjs'
 import VueClipboard from 'vue-clipboard2'
+import ApolloClient from 'apollo-boost'
+import VueApollo from 'vue-apollo'
 import App from './components/App.vue'
 import '../styles/style.scss'
 
 Vue.config.productionTip = false
+const apolloProvider = new VueApollo({
+  defaultClient: new ApolloClient({
+    uri: 'http://127.0.0.1:5000/graphql'
+  })
+})
 Vue.use(VueHighlightJS)
 Vue.use(VueClipboard)
+Vue.use(VueApollo)
 
 Vue.prototype.$chrome = chrome
 
 /* eslint-disable no-new */
-new Vue({
+window.root = new Vue({
+  apolloProvider,
   el: '#root',
   render: h => h(App)
 })
