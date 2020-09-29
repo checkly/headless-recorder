0caf3eb502609a7941675d2d822334c82706f071
Maxi Gimenez <maxi@checklyhq.com>
2020-09-29 13:28:31 +0200

    fix: page.selectOption on playwright

--- src/code-generator/__tests__/PlaywrightCodeGenerator.spec.js
+++ src/code-generator/__tests__/PlaywrightCodeGenerator.spec.js
@@ -6,4 +6,10 @@ describe('PlaywrightCodeGenerator', () => {
     const codeGenerator = new PlaywrightCodeGenerator()
     expect(codeGenerator._parseEvents(events)).toBeFalsy()
   })
+
+  test('it generates a page.selectOption() only for select dropdowns', () => {
+    const events = [{ action: 'change', selector: 'select#animals', tagName: 'SELECT', value: 'hamster' }]
+    const codeGenerator = new PlaywrightCodeGenerator()
+    expect(codeGenerator._parseEvents(events)).toContain(`await page.selectOption('${events[0].selector}', '${events[0].value}')`)
+  })
 })
