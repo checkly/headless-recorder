78b34e359ed7e9a496db80c62f0225b84d699dce
Ignacio Anaya <nacho@checklyhq.com>
2021-06-28 00:32:46 -0300

    refactor: use browser and storage services in popup

--- src/services/storage.js
+++ src/services/storage.js
@@ -4,9 +4,7 @@ export default {
       return Promise.reject('Browser storage not available')
     }
 
-    return new Promise(function(resolve) {
-      chrome.storage.local.get(keys, props => resolve(props))
-    })
+    return new Promise(resolve => chrome.storage.local.get(keys, props => resolve(props)))
   },
 
   set(props) {
@@ -14,8 +12,14 @@ export default {
       return Promise.reject('Browser storage not available')
     }
 
-    return new Promise(function(resolve) {
-      chrome.storage.local.set(props, res => resolve(res))
-    })
+    return new Promise(resolve => chrome.storage.local.set(props, res => resolve(res)))
+  },
+
+  remove(keys) {
+    if (!chrome.storage || !chrome.storage.local) {
+      return Promise.reject('Browser storage not available')
+    }
+
+    return new Promise(resolve => chrome.storage.local.remove(keys, res => resolve(res)))
   },
 }
