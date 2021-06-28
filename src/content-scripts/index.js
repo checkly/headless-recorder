e2f47cfcc4d1a13553b52e3ab6d503259ddad85e
Ignacio Anaya <nacho@checklyhq.com>
2021-06-27 23:36:27 -0300

    refactor: move cs controller and pass store in constructor

--- src/content-scripts/index.js
+++ src/content-scripts/index.js
@@ -1,14 +1,16 @@
 import store from '@/store'
 
 import Overlay from '@/modules/overlay'
-import Recorder from '@/modules/recorder'
 import Shooter from '@/modules/shooter'
-import Controller from '@/modules/controller'
+import Recorder from '@/modules/recorder'
+
+import Controller from '@/content-scripts/controller'
 
 window.headlessRecorder = new Controller({
   shooter: new Shooter(),
   overlay: new Overlay({ store }),
   recorder: new Recorder({ store }),
+  store,
 })
 
 window.headlessRecorder.init()
