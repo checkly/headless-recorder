d99fb68c36a58b983415e8a73dfeb2dbd97c6fe0
Ignacio Anaya <nacho@checklyhq.com>
2021-09-16 16:03:31 -0300

    chore: remove unused shooter from headless controller

--- src/content-scripts/index.js
+++ src/content-scripts/index.js
@@ -1,13 +1,11 @@
 import store from '@/store'
 
 import Overlay from '@/modules/overlay'
-import Shooter from '@/modules/shooter'
 import Recorder from '@/modules/recorder'
 
 import HeadlessController from '@/content-scripts/controller'
 
 window.headlessRecorder = new HeadlessController({
-  shooter: new Shooter(),
   overlay: new Overlay({ store }),
   recorder: new Recorder({ store }),
   store,
