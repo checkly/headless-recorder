83a4042ac375d13c4434308bd6b6e25184f5b980
Pilar Checkly <pilarcheckly@MacBook-Pro-de-Pilar.local>
2021-10-12 16:04:38 -0300

    feat: add storage remove and set test case

--- src/services/__tests__/storage.spec-3.js
+++ src/services/__tests__/storage.spec-3.js
@@ -8,14 +8,23 @@ window.chrome = {
           return cb(store[keys])
         }
 
-        // TODO: verify that keys is an array
         const results = []
-        keys.forEach(key => {
-          results.push(store[key])
-        })
-
-        return cb(results)
+        if (Array.isArray(keys)) {
+          keys.forEach(key => {
+            results.push(store[key])
+          })
+  
+          return cb(results)
+        }
       }),
+      remove: jest.fn((keys, cb) => {
+          delete store[keys];
+          return cb(store)
+      }),
+      set: jest.fn((props, cb) => {
+        const newStore = { ...store, ...props }
+        return cb(newStore)
+    }),
     },
   },
 }
@@ -25,9 +34,13 @@ const store = {
   name: 'lionel',
 }
 
-describe('get', () => {
-  beforeEach(() => {})
+beforeEach(() => {
+  window.chrome.storage.local.get.mockClear()
+  window.chrome.storage.local.set.mockClear()
+  window.chrome.storage.local.remove.mockClear()
+})
 
+describe('get', () => {
   it('return a single value', async () => {
     const token = await storage.get('token')
     expect(token).toBe(store.token)
@@ -38,11 +51,28 @@ describe('get', () => {
     const [token, name] = await storage.get(['token', 'name'])
     expect(token).toBe(store.token)
     expect(name).toBe(store.name)
-    // expect(window.chrome.storage.local.get.mock.calls.length).toBe(1)
+    expect(window.chrome.storage.local.get.mock.calls.length).toBe(1)
   })
 
   it('return undefined when value not found', async () => {
     const nothing = await storage.get('nothing')
     expect(nothing).toBe(undefined)
+  })
+})
+
+describe('remove', () => {
+  it('removes a value', async () => {
+    const store = await storage.remove('token')
+    expect(store.token).toBe(undefined)
+    expect(window.chrome.storage.local.remove.mock.calls.length).toBe(1)
+  })
+})
+
+describe('set', () => {
+  it('set a new value or values', async () => {
+    const newStore = await storage.set({age: 1, country: 2})
+    expect(newStore.age).toBe(1)
+    expect(newStore.country).toBe(2)
+    expect(window.chrome.storage.local.set.mock.calls.length).toBe(1)
   })
 })
